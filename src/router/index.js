c37b5d55df928d39a50bedfdc154d31421605806
Silvia2015 <silvitacabellocatapablo@gmail.com>
2024-06-02 16:49:52 +1200

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-   
+   {
+    path:'/login',
+    name:'login',
+    component: () => import('../views/LoginView.vue')
+   }
   ]
 })
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+ <v-card flat max-width="500" class="mx-auto my-10">
+    <v-card-title class="text-center  text-h4 font-weight-bold" tag="h4">Iniciar sesion</v-card-title>
+    <v-form>
+        <v-text-field type="email" label="Email" class="mb-3"/>
+        <v-text-field type="password" label="Password"/>
+        <v-btn block color="light-green-accent-4">Iniciar Sesion</v-btn>
+    </v-form>
+ </v-card>
+</template>
+
+<style scoped>
+</style>
